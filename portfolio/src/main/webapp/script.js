1bf8a8cef0b4c4167edf54a666e8a7bb79d21d70
Saakshi Saxena <saakshi.saxena2000@gmail.com>
2020-07-15 15:29:28 +0000

    Add new file- gallery.html's link to my index.html. Add pictures to gallery.html. Add about me div in the home page. Delete unwanted lines in my introduction. Delete hello button from index.html and its function in js.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -31,14 +31,3 @@ function addFunFact() {
     
 }
 
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
-
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
-}
