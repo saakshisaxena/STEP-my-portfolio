7357cad5fe3d2ae459e35aef8716b32b27f1f948
Saakshi Saxena <saakshi.saxena2000@gmail.com>
2020-07-28 15:42:10 +0000

    Add upload file form and script to get url
    Add a form in index.html where the user can upload a file/image and also a written message. The 'action' url is not specified from the start as the script fetches it from the /blobstore-upload-url and attaches it to the form, until then our form is hidden.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -144,3 +144,15 @@ function getMaxComments() {
     else
         return(urlParams.get('maxComments'));
 }
+
+function fetchBlobstoreUrlAndShowForm() {
+  fetch('/blobstore-upload-url')
+      .then((response) => {
+        return response.text();
+      })
+      .then((imageUploadUrl) => {
+        const messageForm = document.getElementById('upload-images');
+        messageForm.action = imageUploadUrl;
+        messageForm.classList.remove('hidden');
+      });
+}
